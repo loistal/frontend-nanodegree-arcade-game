a39f6df31c276fbf8c79bfc62bb804415b0f32a7
Lois Talagrand <lois.talagrand@epfl.ch>
2017-02-08 11:03:40 -1000

    Add detailed comments

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-frontend-nanodegree-arcade-game
-===============================
+Frogger-Replica
+===============
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+This web based game is a replica of the classic arcade game "Frogger". Follow the
+instructions below to run and play the game.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+*Run*
+
+
+*Play*
+- Go to: 
+- The goal of Frogger is to make your character (initially located at the bottom) cross
+the "freeway" without colliding with vehicle.
+- Only 4 keyboard keys are allowed: UP, DOWN, LEFT and RIGHT.
+- If your character hits a vehicle, it will automatically reset its position.
+- If you reach the river located on the other side of the road, your victory counter will increase.
--- js/app.js
+++ js/app.js
@@ -1,46 +1,157 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var victoryCounter = 0;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+var canvasWidth = 505;
+var canvasHeight = 606;
+
+var numberHorBlocks = 5;
+var numberVerBlocks = 6;
+
+var blockWidth = 101;
+var blockHeight = 171;
+
+/**
+ * @name Player
+ * @description Character whose position is controlled by the player through keyboard inputs.
+ */
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+
+  // Initial position
+  this.x = 200;
+  this.y = 400;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+
+/**
+ * @name Enemy
+ * @param x initial x position
+ * @param y initial y position
+ * @description Vehicle that the player should avoid in order to win the game. It crosses the canvas from left to right and has a random speed.
+ */
+ var Enemy = function(x, y) {
+  this.sprite = 'images/enemy-bug.png';
+
+  // width of the image in pixels
+  this.width = 107;
+  this.x_radius = this.width / 2;
+
+  // Initial position
+  this.x = x;
+  this.y = y;
+
+  // enemy's speed is randomly chosen in the interval: [100, 600]
+  this.speed = (Math.random() * 500) + 100;
+};
+
+/**
+ * @name update
+ * @description Update the enemy's position
+ * @param dt A time delta between ticks
+ */
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
+  // If the enemy is within the canvas, make it move to the right
+  if(this.x <= (505 + this.width)) {
+    this.x += this.speed * dt;
+  } else { // if the enemy goes off screen, then reset its position
+    this.x = -1 * this.width;
+  }
+
+  // reset the player if he is found within the horizontal radius of the enemy
+  if  (player.x >= this.x - this.x_radius
+    && player.x <= this.x + this.x_radius
+    && player.y >= this.y - this.x_radius
+    && player.y <= this.y + this.x_radius) {
+      player.reset();
+  }
 };
 
-// Draw the enemy on the screen, required method for game
+/**
+ * @name render
+ * @description Renders the enemy
+ */
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+/**
+ * @name reset
+ * @description Resets the player's position
+ */
+Player.prototype.reset = function() {
+  this.x = (blockWidth * numberHorBlocks / 2) - blockWidth / 2;
+  this.y = (blockHeight * numberVerBlocks / 2) - blockHeight / 1.5 + 1;
+};
 
+/**
+ * @name render
+ * @module Player
+ * @description Renders the player
+ */
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/**
+ * @name update
+ * @module Player
+ * @description Updates player's position while making sure to keep him within the canvas
+ */
+Player.prototype.update = function() {
 
+  // handle input keys and make sure that the player stays within the canvas  
+  if(this.ctlKey === 'left' && this.x >= 0){
+    this.x = this.x - blockWidth;
+  } else if(this.ctlKey === 'right' && this.x < 400){
+    this.x = this.x + blockWidth;
+  } else if(this.ctlKey === 'up'){
+    this.y = this.y - blockHeight/2;
+  } else if (this.ctlKey === 'down' && this.y < 400){
+    this.y = this.y + blockHeight/2;
+  }
+  this.ctlKey = null;
 
+  // Reset player's position if he managed to reach the river
+  if(this.y < 25){
+    this.reset();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+    // update and display the victory counter
+    victoryCounter++;
+    $('p').html("Points: " + victoryCounter);
+  }
+};
+
+/**
+ * @name handleInput
+ * @param event
+ * @description Update to the latest key that was pressed
+ */
+Player.prototype.handleInput = function(event) {
+  this.ctlKey = event;
+};
+
+var player = new Player();
+var allEnemies = [];
+
+// Define y coordinates of the vehicle rows
+var yFirstRow = 230;
+var ySecondRow = 142;
+var yThirdRow = 57;
+
+// Add the enemies
+allEnemies.push(new Enemy(-2, yFirstRow));
+allEnemies.push(new Enemy(-2, yFirstRow));
+allEnemies.push(new Enemy(-2, ySecondRow));
+allEnemies.push(new Enemy(-2, yThirdRow));
+allEnemies.push(new Enemy(-2, yThirdRow));
+
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
